ab8cfe292a0244c038a0dbde2321f3c64a3e4d66
princesmaytaurismacahya <princesmaytaurismacahya@gmail.com>
2024-09-10 08:00:57 +0700

    Add files via upload

---
+++ App.py
@@ -0,0 +1,8 @@
+#Hitung Luas Segitiga sederhana
+def luas_segitiga():
+    a = int(input("Masukkan alas segitiga: "))
+    t = int(input("Masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("Luas segitga adalah: ", luas)
+
+    luas_segitiga()
